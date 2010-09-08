76e91c335c9f6a8812ae4d7eac6491f97ae4f90b
Rolly Maulana Awangga <rolly@awang.ga>
2010-09-08 15:36:37 +0700

    matlab 2010 compatibiliti

--- generateFigures.m
+++ generateFigures.m
@@ -1,4 +1,4 @@
-figuresfolder=".\main\data\s1_figures\"
+figuresfolder='.\main\data\s1_figures\'
 
 if ~exist(figuresfolder, 'dir')
        mkdir(figuresfolder)
@@ -9,6 +9,6 @@ while n<1152
     offsetOfFigureRun = min(find(runIDs == n));
     s = reshape(D.label(offsetOfFigureRun + 10, 2:end),10,10);
     imagesc(s); colormap(gray); axis image;
-    saveas(gcf,strcat(figuresfolder,int2str(n),".png"))
+    saveas(gcf,strcat(figuresfolder,int2str(n),'.png'))
     n=n+1
 end
