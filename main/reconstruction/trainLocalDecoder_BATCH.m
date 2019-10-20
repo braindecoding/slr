75d1c3c5d986f8d52f02396c59e107740e9f3726
Rolly Maulana Awangga <rolly@awang.ga>
2019-10-20 14:15:47 +0700

    change ROI type

--- main/reconstruction/trainLocalDecoder_BATCH.m
+++ main/reconstruction/trainLocalDecoder_BATCH.m
@@ -13,13 +13,13 @@ trainLocalDecoder('s1_s1071119',{'1x1'},'V1V2','leave1',0);
 
 %% or juist write as follows,
 
-%trainLocalDecoder('s1_s1071119',{'1x1','1x2','2x1','2x2'},'V1V2','leave0',0);
-%trainLocalDecoder('s1_s1071119',{'1x1','1x2','2x1','2x2'},'V1V2','leave1',0);
+trainLocalDecoder('s1_s1071119',{'1x1','1x2','2x1','2x2'},'V1V2','leave0',0);
+trainLocalDecoder('s1_s1071119',{'1x1','1x2','2x1','2x2'},'V1V2','leave1',0);
 
-%trainLocalDecoder('s1_s1071119',{'1x1','1x2','2x1','2x2'},'V1V2','leave0',0);
-%trainLocalDecoder('s1_s1071119',{'1x1','1x2','2x1','2x2'},'V1V2','leave1',0);
+trainLocalDecoder('s1_s1071119',{'1x1','1x2','2x1','2x2'},'V1','leave0',0);
+trainLocalDecoder('s1_s1071119',{'1x1','1x2','2x1','2x2'},'V1','leave1',0);
 
-%trainLocalDecoder('s1_s1071119',{'1x1','1x2','2x1','2x2'},'V1V2','leave0',0);
-%trainLocalDecoder('s1_s1071119',{'1x1','1x2','2x1','2x2'},'V1V2','leave1',0);
+trainLocalDecoder('s1_s1071119',{'1x1','1x2','2x1','2x2'},'V2','leave0',0);
+trainLocalDecoder('s1_s1071119',{'1x1','1x2','2x1','2x2'},'V2','leave1',0);
 
 
