f99cafde1f391068b895e0563a6dba65bcde976b
Rolly Maulana Awangga <rolly@awang.ga>
2021-01-10 05:19:17 +0700

    updates

---
+++ main/reconstruction/nnmodel/bdtb.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  8 17:15:32 2021
+
+@author: rolly
+"""
+from tensorflow.keras.models import Sequential,load_model
+from tensorflow.keras.layers import Dense
+import numpy as np
+import matplotlib.pyplot as plt
+
+def loaddatanorest(mat):
+    mdata =mat['D']
+    mdtype = mdata .dtype 
+    ndata = {n: mdata[n][0, 0] for n in mdtype.names}
+    label = ndata['label']
+    data = ndata['data']
+    nl=[]
+    nd=[]
+    for l,d in zip(label,data):
+        if l[1] < 2:
+            nl.append(l)
+            nd.append(d)
+    return nl,nd
+
+def loadtestandlabel(mat):
+    nl,nd=loaddatanorest(mat)
+    label=nl[440:]
+    data=nd[440:]
+    return np.asarray(data, dtype=np.float64),np.asarray(label, dtype=np.float64)
+    
+def loadtrainandlabel(mat):
+    nl,nd=loaddatanorest(mat)
+    alllabel=nl[:440]
+    rdata=nd[:440]
+    return np.asarray(rdata, dtype=np.float64),np.asarray(alllabel, dtype=np.float64)
+
+def getlabel(alllabel,x):
+    px1=[]
+    for i in alllabel:
+        px1.append(i[x])        
+    label_data=np.asarray(px1, dtype=np.float64)
+    return label_data
+
+#https://machinelearningmastery.com/tutorial-first-neural-network-python-keras/
+def createmodel(train_data,label_data,filename):
+    X = train_data / 5.0
+    y = label_data
+    # define the keras model
+    model = Sequential()
+    model.add(Dense(1450, input_dim=967, activation='relu'))
+    model.add(Dense(967, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    # compile the keras model
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.fit(X, y, epochs=150, batch_size=10)
+    # evaluate the keras model
+    _, accuracy = model.evaluate(X, y)
+    print('Accuracy: %.2f' % (accuracy*100))
+    model.save(str(filename))
+    
+def generatePixel(pxpath,data):
+    model = load_model(pxpath)
+    return model.predict_classes(data)
+
+def showFig(az):
+    gbr = az.reshape((10,10)).T
+    plt.imshow(gbr)
+    
+def saveFig(az,fname):
+    data = az.reshape((10,10)).T
+    new_data = np.zeros(np.array(data.shape) * 10)
+    for j in range(data.shape[0]):
+        for k in range(data.shape[1]):
+            new_data[j * 10: (j+1) * 10, k * 10: (k+1) * 10] = data[j, k]
+    plt.imsave(str(fname),new_data)
+    
+def delfirstCol(testlb):
+    return np.delete(testlb,0,1)
+
+    
---
+++ main/reconstruction/nnmodel/multivoxelpattern.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  8 04:38:55 2021
+
+@author: rolly
+"""
+import bdtb
+import scipy.io
+
+matfile='../de_s1_AllArea_Ecc1to11_baseByRestPre_smlr_s1071119ROI_resol10_leave0_1x1_preprocessed.mat'
+#matfile='../de_s1_V1_Ecc1to11_baseByRestPre_smlr_s1071119ROI_resol10_leave0_1x1_preprocessed.mat'
+mat = scipy.io.loadmat(matfile)
+
+
+# In[]: load data
+train_data,label=bdtb.loadtrainandlabel(mat)
+# In[]: train and save mmodel
+for x in range(1,101):
+    labelperpx=bdtb.getlabel(label,x)
+    bdtb.createmodel(train_data,labelperpx,'.\\1x1\\label'+str(x))
+
+
+# In[]: load data norest
+a,b=bdtb.loaddatanorest(mat)
+
+# In[]: load data norest
+testdt,testlb=bdtb.loadtestandlabel(mat)
+
+# In[]: predict
+
+import numpy as np
+
+pixel=1
+piksel=bdtb.generatePixel('.\\1x1\\label'+str(pixel),testdt)
+for x in range(2,101):
+    pikselbr=bdtb.generatePixel('.\\1x1\\label'+str(x),testdt)
+    piksel=np.concatenate((piksel,pikselbr),axis=1)
+    
+# In[]:
+    
+
+z=bdtb.delfirstCol(testlb)
+n=1
+for i in z:
+    bdtb.saveFig(i,'.\\fig\\'+str(n)+'.png')
+    n=n+1
+    
+# In[]:
+
+n=1
+for i in piksel:
+    bdtb.saveFig(i,'.\\figrec\\'+str(n)+'.png')
+    n=n+1
+    
+
