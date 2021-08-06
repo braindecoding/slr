bf595239c5d242033818fa8367f6b9aaea6cca9e
Rolly Maulana Awangga <rolly@awang.ga>
2021-08-07 06:28:44 +0700

    ssim

--- main/reconstruction/nnmodel/bdtb.py
+++ main/reconstruction/nnmodel/bdtb.py
@@ -300,3 +300,28 @@ def ubahkelistofchunks(mse,msem,pred,predm,label,n):
     lpredm=list(divide_chunks(predm, n))
     llabel=list(divide_chunks(label, n))
     return lmse,lmsem,lpred,lpredm,llabel
+
+def ssimscore(gambar1,gambar2):
+    # Usage:
+    #
+    # python3 script.py --input original.png --output modified.png
+    # Based on: https://github.com/mostafaGwely/Structural-Similarity-Index-SSIM-
+    
+    # 1. Import the necessary packages
+    from skimage.measure import compare_ssim
+    import cv2
+
+    imageA = cv2.imread(gambar1)
+    imageB = cv2.imread(gambar2)
+    
+    # 4. Convert the images to grayscale
+    grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
+    grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
+    
+    # 5. Compute the Structural Similarity Index (SSIM) between the two
+    #    images, ensuring that the difference image is returned
+    (score, diff) = compare_ssim(grayA, grayB, full=True)
+    diff = (diff * 255).astype("uint8")
+    
+    # 6. You can print only the score if you want
+    print("SSIM: {}".format(score))
