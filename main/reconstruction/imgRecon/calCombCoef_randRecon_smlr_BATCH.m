2aec77f33332fcfbbea615b88e0a12194d63e9aa
Rolly Maulana  Awangga <rolly@awang.ga>
2022-04-19 15:24:49 +0700

    remove

--- main/reconstruction/imgRecon/calCombCoef_randRecon_smlr_BATCH.m
+++ main/reconstruction/imgRecon/calCombCoef_randRecon_smlr_BATCH.m
@@ -1,10 +1,10 @@
-roiAreaList = {'V1V2'};
+roiAreaList = {'V1'};
 
 for i = 1:length(roiAreaList)
     for imgTestRunIdx = 1:10
     
-        calCombCoef_randRecon_smlr('1x1','errFuncImageNonNegCon',imgTestRunIdx, roiAreaList{i})
-        %calCombCoef_randRecon_smlr('1x1','no_opt','imgTestRunIdx, roiAreaList{i})
+        %calCombCoef_randRecon_smlr('1x1','errFuncImageNonNegCon',imgTestRunIdx, roiAreaList{i})
+        calCombCoef_randRecon_smlr('1x1','no_opt',imgTestRunIdx, roiAreaList{i})
         %calCombCoef_randRecon_smlr('1x1_1x2_2x1_2x2','errFuncImageNonNegCon',imgTestRunIdx, roiAreaList{i})
         %calCombCoef_randRecon_smlr('1x2','errFuncImageNonNegCon',imgTestRunIdx, roiAreaList{i})
         %calCombCoef_randRecon_smlr('2x1','errFuncImageNonNegCon',imgTestRunIdx, roiAreaList{i})
